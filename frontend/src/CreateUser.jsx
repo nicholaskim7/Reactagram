6a3102ce4fccc9d05284df90fe76b32e7948a9cd
nicholaskim7 <154484441+nicholaskim7@users.noreply.github.com>
2024-08-05 18:51:10 -0500

    Update CreateUser.jsx Added styling

--- frontend/src/CreateUser.jsx
+++ frontend/src/CreateUser.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import './Login.css';
 
 function CreateUser() {
     const [name, setName] = useState('')
@@ -27,7 +28,7 @@ function CreateUser() {
     }
 
   return (
-    <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
+    <div className='d-flex vh-100 bg-light-blue justify-content-center align-items-center'>
         <div className='w-50 bg-white rounded p-3'>
             <form onSubmit={handleSubmit}>
                 <h2>Add User</h2>
