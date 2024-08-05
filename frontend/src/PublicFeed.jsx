1df6fecd144c72ded469d7bd4975952364a74d01
nicholaskim7 <154484441+nicholaskim7@users.noreply.github.com>
2024-08-05 18:47:24 -0500

    Update PublicFeed.jsx Added styling

--- frontend/src/PublicFeed.jsx
+++ frontend/src/PublicFeed.jsx
@@ -1,5 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import './Login.css'
+import './Feed.css'
+import { Link } from 'react-router-dom';
+
+const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString(); // Adjust format if needed
+};
 
 function PublicFeed() {
     const [posts, setPosts] = useState([]);
@@ -21,22 +29,22 @@ function PublicFeed() {
 
     return (
         <div>
-            <div>For You Page (aka fetching all posts... no algo)</div>
-            <div className='d-flex flex-column align-items-center'>
+            <div className='d-flex flex-column align-items-center bg-light-blue'>
                 <div className='mt-4 w-50 rounded p-3 custom-box'>
                     <div className="mb-2">
                         {posts.map(post => (
                             <div key={post.ID} className="post">
-                                <h6>@{post.username}</h6> {/* Display the username */}
+                                <h5><Link to={`/publicprofile/${post.username}`} className="post-link">@{post.username}</Link></h5> {/* Display the username */}
                                 <h3>{post.Text}</h3>
                                 {JSON.parse(post.Images).map((imageUrl, index) => (
                                     <img
                                         key={index}
                                         src={`http://localhost:8081${imageUrl}`}
                                         alt="Post"
-                                        style={{ width: '200px', height: 'auto', margin: '10px' }}
+                                        style={{ width: '350px', height: 'auto', margin: '10px' }}
                                     />
                                 ))}
+                                <h6>{formatDate(post.date)}</h6>
                             </div>
                         ))}
                     </div>
