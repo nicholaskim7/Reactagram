f7128ca24129f3ba9a4b404f7aeae6bb1da5e1be
nicholaskim7 <154484441+nicholaskim7@users.noreply.github.com>
2024-08-12 21:26:39 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,11 +7,8 @@ import In from './UserLoggedin';
 import Public from './PublicProfile';
 import CreateUser from './CreateUser';
 import UpdateUser from './UpdateUser';
-import Navbar from './Navibar';
 import Navbar2 from './Navibar2';
 import Profile from './UserProfile';
-import Photos from './photos';
-import Feed from './Feed';
 import PublicFeed from './PublicFeed';
 import Draw from './Draw';
 import FollowersList from './Followers';
@@ -33,8 +30,6 @@ function App() {
           <Route path='/create' element={<CreateUser/>}></Route>
           <Route path='/update/:id' element={<UpdateUser/>}></Route>
           <Route path='/loggedin/updateprofile/:id' element={<Profile />}></Route>
-          <Route path='/photos' element={<Photos />}></Route>
-          <Route path='/feed' element={<Feed />}></Route>
           <Route path='/posts' element={<PublicFeed />}></Route>
           <Route path='/draw' element={<Draw />}></Route>
           <Route path="/followers/:id" element={<FollowersList />} />
