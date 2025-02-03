7fcfad852241aa8266a0a72f270e08d9a0a38ef9
nicholas kim <154484441+nicholaskim7@users.noreply.github.com>
2025-02-03 16:33:16 -0600

    Update Welcome.jsx "Style"

--- frontend/src/Welcome.jsx
+++ frontend/src/Welcome.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import './Login.css';
+import './Navibar.css';
+import './Welcome.css';
 
 function Welcome() {
   return (
-    <div>
-        <h2>Welcome to Scuffed Insta</h2>
-        <h5>To continue please login</h5>
-        <Link as={Link} to={"/login"} className='btn btn-primary'>Login or Sign Up</Link>
+    <div className="welcome-container d-flex flex-column justify-content-center align-items-center text-center">
+        <h2 className="welcome-title">Welcome to React-agram</h2>
+        <h5 className="welcome-subtitle">To continue, please login</h5>
+        <Link as={Link} to={"/login"} className='btn btn-primary welcome-btn'>Login or Sign Up</Link>
     </div>
   )
 }
